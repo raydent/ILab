5118dadada6d953f2586714492d010ed0730e8cd
raydent <benisgachi@gmail.com>
2018-11-13 17:57:43 +0300

    few fixes

--- Onegin.c
+++ Onegin.c
@@ -16,7 +16,7 @@ int ReadToNewStrings(char *giantstring, StringContainer* newstrings, int filesiz
 int compforward(const StringContainer*, const StringContainer*);
 int compbackward(const StringContainer*, const StringContainer*);
 void WriteToFile(FILE* filetowrite, StringContainer*   newstrings, int stringcount);
-char CorrectLetter(int i, const StringContainer* string);
+char CorrectLetter(int numb, const StringContainer* string);
 
 int main()
 {
@@ -32,7 +32,7 @@ int main()
   ReadToString(filetoread, giantstring, filesize);
   int stringcount = ReadToNewStrings(giantstring, newstrings, filesize);
   qsort(newstrings, stringcount, sizeof(StringContainer), (int(*) (const void *, const void *)) compforward);
-  // WriteToFile(filetowrite, newstrings, stringcount);
+  WriteToFile(filetowrite, newstrings, stringcount);
   qsort(newstrings, stringcount, sizeof(StringContainer), (int(*) (const void *, const void *)) compbackward);
   WriteToFile(filetowrite, newstrings, stringcount);
   fclose(filetoread);
@@ -86,31 +86,31 @@ int ReadToNewStrings(char *giantstring, StringContainer* newstrings, int filesiz
 int compforward(const StringContainer* string1, const StringContainer* string2)
 {
   char letter1 = 0, letter2 = 0;
-  int i1 = 0, i2 = 0;
+  int numb1 = 0, numb2 = 0;
   for (;;)
   {
-    if ((string1->pointer[i1] == 0) && (string2->pointer[i2] == 0))
+    if ((string1->pointer[numb1] == 0) && (string2->pointer[numb2] == 0))
       return 0;
-    if (string1->pointer[i1] == 0)
+    if (string1->pointer[numb1] == 0)
       return -1;
-    if (string2->pointer[i2] == 0)
+    if (string2->pointer[numb2] == 0)
       return 1;
-    while ((string1->pointer[i1]) < 65 || ((string1->pointer[i1]) > 90 && (string1->pointer[i1]) < 97) || ((string1->pointer[i1]) > 122))
+    while ((string1->pointer[numb1]) < 65 || ((string1->pointer[numb1]) > 90 && (string1->pointer[numb1]) < 97) || ((string1->pointer[numb1]) > 122))
     {
-      i1++;
+      numb1++;
     }
-    while ((string2->pointer[i2]) < 65 || ((string2->pointer[i2]) > 90 && (string2->pointer[i2]) < 97) || (string2->pointer[i2]) > 122)
+    while ((string2->pointer[numb2]) < 65 || ((string2->pointer[numb2]) > 90 && (string2->pointer[numb2]) < 97) || (string2->pointer[numb2]) > 122)
     {
-      i2++;
+      numb2++;
     }
-    letter1 = CorrectLetter(i1, string1);
-    letter2 = CorrectLetter(i2, string2);
+    letter1 = CorrectLetter(numb1, string1);
+    letter2 = CorrectLetter(numb2, string2);
     if (letter1 > letter2)
       return 1;
     if (letter1 < letter2)
       return -1;
-    i1++;
-    i2++;
+    numb1++;
+    numb2++;
   }
 }
 
@@ -118,43 +118,43 @@ int compbackward(const StringContainer* string1, const StringContainer* string2)
 {
 
   char letter1 = 0, letter2 = 0;
-  int i1 = string1->charnum, i2 = string2->charnum;
+  int numb1 = string1->charnum, numb2 = string2->charnum;
   for (;;)
   {
-    while ((string1->pointer[i1]) < 65 || ((string1->pointer[i1]) > 90 && (string1->pointer[i1]) < 97) || ((string1->pointer[i1]) > 122))
+    while ((string1->pointer[numb1]) < 65 || ((string1->pointer[numb1]) > 90 && (string1->pointer[numb1]) < 97) || ((string1->pointer[numb1]) > 122))
     {
-      i1--;
+      numb1--;
     }
-    while ((string2->pointer[i2]) < 65 || ((string2->pointer[i2]) > 90 && (string2->pointer[i2]) < 97) || (string2->pointer[i2]) > 122)
+    while ((string2->pointer[numb2]) < 65 || ((string2->pointer[numb2]) > 90 && (string2->pointer[numb2]) < 97) || (string2->pointer[numb2]) > 122)
     {
-      i2--;
+      numb2--;
     }
-    letter1 = CorrectLetter(i1, string1);
-    letter2 = CorrectLetter(i2, string2);
+    letter1 = CorrectLetter(numb1, string1);
+    letter2 = CorrectLetter(numb2, string2);
     if (letter1 > letter2)
       return 1;
     if (letter1 < letter2)
       return -1;
-    if ((i1 == 0) && (i2 == 0))
+    if ((numb1 == 0) && (numb2 == 0))
       return 0;
-    if (i1 == 0)
+    if (numb1 == 0)
       return -1;
-    if (i2 == 0)
+    if (numb2 == 0)
       return 1;
-    i1--;
-    i2--;
+    numb1--;
+    numb2--;
   }
 }
-char CorrectLetter(int i, const StringContainer* string)
+char CorrectLetter(int numb, const StringContainer* string)
 {
   char letter = 0;
-  if (((string->pointer[i]) >= 65) && (string->pointer[i] <= 90))
+  if (((string->pointer[numb]) >= 65) && (string->pointer[numb] <= 90))
   {
-    letter = (string->pointer[i]) + 32;
+    letter = (string->pointer[numb]) + 32;
   }
-  if ((string->pointer[i] >= 97) && (string->pointer[i] <= 122))
+  if ((string->pointer[numb] >= 97) && (string->pointer[numb] <= 122))
   {
-    letter = string->pointer[i];
+    letter = string->pointer[numb];
   }
   return letter;
 }
